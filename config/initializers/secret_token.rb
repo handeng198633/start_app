5fc6c3aa6db922318c5421ac5a5fae9267cfc47c
hdeng <han.deng@ansys.com>
2015-04-29 17:28:54 +0800

    Add a staticPages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,4 @@
+# Be sure to restart your server when you modify this file.
+
+StartApp::Application.config.secret_key_base = 'f4ef05e428449a72143c76e7b10c57d1c951226fad7f3f581b9fe93e02a3ce04d93d92c331587dbb
+d39ca9bb1e99effa46a0d43a12814e3cb5d8c474c13ad4c5'
