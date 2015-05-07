46917105d0ab342b429ef04be9926b40f8dd22e5
hdeng <han.deng@ansys.com>
2015-05-07 16:30:25 +0800

    make a basic user model and secure password

--- Gemfile
+++ Gemfile
@@ -6,6 +6,9 @@ gem 'rails', '4.0.0'
 # Use sqlite3 as the database for Active Record
 
 gem 'bootstrap-sass', '2.3.2.0'
+#hash function for password
+gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'bcrypt'
 
 group :development, :test do
 	gem 'sqlite3', '1.3.10'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+	#call back and make all mail address downcase before saved
+	before_save { self.email = email.downcase }
+	validates :name, presence: true, length: { maximum:15 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, 
+					format: { with: VALID_EMAIL_REGEX }, 
+					uniqueness: { case_sensitive: false }
+
+#all about password secure is add to :has_secure_password	
+	has_secure_password
+	validates :password, length:{ minimum: 6, maximum: 20 }
 end
