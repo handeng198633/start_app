a25ebee9d864fc8162259d2b378071a7803f1eba
hdeng <han.deng@ansys.com>
2015-05-14 17:32:20 +0800

    update user for right control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,27 @@
 class UsersController < ApplicationController
-  
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def show
   	@user = User.find(params[:id])
   end
   
   def new
   	@user = User.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      #Handle a successful update
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -24,4 +40,16 @@ class UsersController < ApplicationController
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
+
+    def signed_in_user
+      unless signed_in?
+        flash[:notice] = "Please sign in"
+        redirect_to signin_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end
