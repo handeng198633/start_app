6b08ad687734a02c8cbb11a4f3f963958125037f
hdeng <han.deng@ansys.com>
2015-05-22 16:40:43 +0800

    Finish following user part except dynamic list function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ActiveRecord::Base
 	#call back and make all mail address downcase before saved
 	has_many :microposts, dependent: :destroy
+
 	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
 	has_many :followed_users, through: :relationships, source: :followed
+
+	has_many :reverse_relationships, foreign_key: "followed_id", 
+									class_name: "Relationship", 
+									dependent: :destroy
+	has_many :followers, through: :reverse_relationships, source: :follower
 
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
@@ -29,12 +35,17 @@ class User < ActiveRecord::Base
 	end
 
 	def following?(other_user)
-		
+		relationships.find_by(followed_id: other_user.id)
 	end
 
 	def follow!(other_user)
-		
+		relationships.create!(followed_id: other_user.id)
 	end
+
+	def unfollow!(other_user)
+		relationships.find_by(followed_id: other_user.id).destroy
+	end
+
 	private
 
 		def create_remember_token
